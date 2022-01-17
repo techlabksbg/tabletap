a78b97939744db9dee5460e88d6f674bde52edfb
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2022-01-17 15:06:37 +0100

    added some comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,28 @@
 #include <Arduino.h>
 #include "xtensa/core-macros.h"
 
+// GPIO pins, on which the timings should be measured.
 #define PIN0 12
 #define PIN1 13
 #define PIN2 14
 
 int pins[] = {PIN0, PIN1, PIN2};
+// Test pins (to test the code, directly wire pin 21 to 12, etc.)
 int test[] = {21,22,23};
+
+// Contains the number of ticks when the GPIO pin was raised
 volatile unsigned long fired[3];
+// Number of measured pins (each pin is only measured once)
 volatile int numDone = 0;
 
+// Interupt routine: Store time, increase number of measured pins, disable interrupt on this pin
 #define itr(X) void IRAM_ATTR raisingPin##X() { detachInterrupt(PIN##X); fired[X] = XTHAL_GET_CCOUNT(); numDone++; }
+// define 3 interrupt functions:
 itr(0)
 itr(1)
 itr(2)
 
+// function call to attch interrupts
 #define attachp(X) attachInterrupt(PIN##X, raisingPin##X, RISING);
 void einschalten() {
   numDone = 0;
@@ -23,6 +31,7 @@ void einschalten() {
   attachp(2);
 }
 
+// not used for now, detach all interrupts
 void ausschalten() {
   for (auto pin: pins) {
     detachInterrupt(pin);
@@ -30,7 +39,7 @@ void ausschalten() {
   numDone = 0;
 }
 
-// Wait for 1ms for every pin to be 0
+// Wait for every pin to be LOW for at least 1ms
 void coolDown() {
   unsigned long us = micros();
   while (micros()-us<1000) {
@@ -49,6 +58,7 @@ void setup() {
     pinMode(pin, OUTPUT);
     digitalWrite(pin, LOW);
   }
+  // set up all interrupts
   einschalten();
 
 }
@@ -62,15 +72,16 @@ void auswerten() {
     if (fired[i]<fired[first]) first = i;
   }
   for (int i=0; i<3; i++) {
-    Serial.printf("Pin %d fired at %lu\n", pins[i], fired[i]-fired[first]);
+    Serial.printf("Pin %d fired at %lu ticks -> %lu us\n", pins[i], fired[i]-fired[first], (fired[i]-fired[first])/COUNTSPERUSECOND);
   }
 }
 
+// Fire the test pins at some random us, and estimate the ticks
 void fireTest() {
   unsigned long testus[] = {rand()%500+50, rand()%500+50, rand()%500+50};
   testus[rand()%3] = 0;
   for (int i=0; i<3; i++) {
-    Serial.printf("Preparing pin %d at %lu us -> %lu ticks\n", pins[i], testus[i], testus[i]*240);
+    Serial.printf("Preparing pin %d at %lu us -> %lu ticks\n", pins[i], testus[i], testus[i]*COUNTSPERUSECOND);
   }
   unsigned long us = micros();
   int f = 0;
@@ -90,7 +101,7 @@ void fireTest() {
 
 unsigned long ms = 10000L;
 void loop() {
-  if (numDone==3) {
+  if (numDone==3) {  // All three pins have fired?
     auswerten();
     coolDown();
     einschalten();
